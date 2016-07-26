4a9d0fe0d2754fe3bba1ffd480d23238130a44a8
SarahJaine <sarahjaine.sz@gmail.com>
2016-07-26 00:06:09 -0400

    added cv models to context view, added cv template tags to cv template

--- jordanbeach/views.py
+++ jordanbeach/views.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.views.generic import TemplateView, ListView
-from .models import Publication
+from .models import Teaching, Award, Poster, Talk, Publication
 
 
 class HomeView(TemplateView):
@@ -11,3 +11,12 @@ class HomeView(TemplateView):
 class CVView(ListView):
     model = Publication
     template_name = 'cv.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CVView, self).get_context_data(**kwargs)
+        context['mentor_list'] = Teaching.objects.filter(role='mentor')
+        context['instructor_list'] = Teaching.objects.filter(role='instructor')
+        context['award_list'] = Award.objects.all()
+        context['poster_list'] = Poster.objects.all()
+        context['talk_list'] = Talk.objects.all()
+        return context
